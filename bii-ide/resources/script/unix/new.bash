a9f6b135304c189c36f3a02238927d792dbc6828
David <david.sanchez@biicode.com>
2014-05-14 16:01:48 +0200

    unix script updated

--- bii-ide/resources/script/unix/new.bash
+++ bii-ide/resources/script/unix/new.bash
@@ -3,3 +3,4 @@ echo "Enter Project name: "
 read NAME
 bii new $NAME
 read -p "press to continue" any_key
+exit
--- bii-ide/resources/script/unix/setup.bash
+++ bii-ide/resources/script/unix/setup.bash
@@ -1,3 +1,4 @@
 #!bin/bash
 bii setup:arduino
 read -p "press to continue" any_key
+exit
